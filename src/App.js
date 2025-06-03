ba32dd3aa31d69268a27655f71b128b72ce6c8a9
doddivenkatesh <doddivenkatesh145@gmail.com>
2025-06-03 18:56:21 +0530

    crudoperations

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 
 import './App.css';
 import User from './getuser/User';
-// import ModelView from './Components/ModelView'
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+ //import ModelView from './Components/ModelView'
+ //import { BrowserRouter as Router, Routes, Route, RouterProvider, createBrowserRouter } from 'react-router-dom';
+ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AddUser from './adduser/AddUser';
+import Update from './updateUser/Update';
+
 function App() {
+
+  const route = createBrowserRouter([
+    {
+      path:"/",
+      element: <User />
+    },
+    {
+      path:"/add",
+      element: <AddUser/>
+    },
+    {
+      path:"/update/:id",
+      element: <Update/>
+    }
+  ])
+  
+
   return (
-    <div className="App">
-      <User/>
+    <div >
+      <RouterProvider router={route}></RouterProvider>
       
-    {/* <Router>
+    
+      
+     {/* <Router>
       <Routes>
-          <Route path="/" element={<ModelView />} />
+          <Route path="/adduser" element={<User />} />
+          <Route path="/" element={<AddUser />} />
       </Routes>
-    </Router> */}
+    </Router>  */}
     </div>
   );
 }
--- src/getuser/User.jsx
+++ src/getuser/User.jsx
@@ -1,56 +1,100 @@
-import React from 'react'
-import './user.css'
-import { useState } from 'react'
+import "./user.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import toast from "react-hot-toast";
+
 const User = () => {
+  const [users, setUsers] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:8080/api/users", {
+          // withCredentials: true,
+          // headers: {
+          //     'Content-Type': 'application/json',
+          //     'Access-Control-Allow-Origin': '*',
+          //     'Access-Control-Allow-Credentials': true
+          // }
+        });
+        setUsers(response.data);
+      } catch (error) {
+        console.error("Error while fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
 
-    const [users,setUsers] = useState([]);
+  const deleteUser = async (userId) => {
+    await axios
+      .delete(`http://localhost:8080/api/users/${userId}`)
+      .then((response) => {
+        setUsers((previousUsers) =>
+          previousUsers.filter((user) => user.id !== userId)
+        );
+        toast.success("User Deleted Successfully", { position: "top-right" });
+        toast.success(response.data.message, { position: "top-right" });
+      })
 
-    useEffect( () =>{
-        const fetchData = async () =>{
-
-            try{
-                 await get("http://localhost:8080/api/users")
-            }catch(error){
-                console.error("Error while fetching data:", error);
-            }
-        }
-    })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
-    <div className='userTable'>
-        <button type="button" class="btn btn-primary">Add User <i class="fa-solid fa-user-plus"></i></button>
-         <table className='table table-bordered'>
+    <div className="userTable">
+      <Link to="/add" type="button" class="btn btn-primary">
+        Add User <i class="fa-solid fa-user-plus"></i>
+      </Link>
 
-            <thead>
-                <tr>
-                    <th scope='col'>S.No</th>
-                    <th scope='col'>Name</th>
-                    <th scope='col'>Email</th>
-                    <th scope='col'>Address</th>
-                    <th scope='col'>Actions</th>
-                    
+      {users.length === 0 ? (
+        <div className="noData">
+          <h3>No Users Found</h3>
+          <p>Please add a user to see the list.</p>
+        </div>
+      ) : (
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th scope="col">S.No</th>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Address</th>
+              <th scope="col">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => {
+              return (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>{user.address}</td>
+                  <td className="actionButtons">
+                    <Link
+                      to={`/update/` + user.id}
+                      type="button"
+                      className="btn btn-info"
+                    >
+                      <i className="fa-solid fa-pen-to-square"></i>
+                    </Link>
+                    <button
+                      type="button"
+                      onClick={() => deleteUser(user.id)}
+                      className="btn btn-danger"
+                    >
+                      <i className="fa-solid fa-trash"></i>
+                    </button>
+                  </td>
                 </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Leanne Graham</td>
-                    <td>bret@gamil.com
-                    </td>
-                         <td>canada</td>
-                              <td className='actionButtons'>
-                                <button type="button" class="btn btn-info"> <i class="fa-solid fa-pen-to-square"></i></button>
-                               
-                                <button type="button" class="btn btn-danger"> <i class="fa-solid fa-trash"></i></button>
-                                <i class="fa-solid fa-trash"></i>
-                                </td>
-                
-                </tr>
-            </tbody>
-         </table>
-     
+              );
+            })}
+          </tbody>
+        </table>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default User
+export default User;
